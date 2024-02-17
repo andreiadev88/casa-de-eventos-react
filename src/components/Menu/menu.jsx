5fbd26a6ca0dacd6303d83a679d15b860a9daddb
dos Santos <a.dos.santos@sap.com>
2024-02-17 00:36:12 -0300

    menu finalizado

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,24 +1,39 @@
 import styled from "styled-components";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const MenuContainer = styled.div`
-
+  width: calc(100% - 250px);
+  heigth: 120px;
+  display: flex;
+  justify-content: right;
+  aline-itens: center;
 `
-const MenuList = styled.ul``
+const MenuList = styled.ul`
+  display: flex;
+`
 
-const MenuItem = styled.li``
+const MenuItem = styled.li`
+  list-style-type: none;
+`
 
-const MenuLink =styled(Link)``
+const MenuLink = styled(Link)`
+  color: #FEFAE0;
+  text-decoration: none;
+  padding: 0 15px;
+  &:hover{
+    color: #B99470;
+  }
+`
 
 function Menu() {
     return (
         <MenuContainer>
             <MenuList>
                 <MenuItem>
-                  <MenuLink to="/homepage">Homepage</MenuLink>
+                    <MenuLink to="/homepage">Homepage</MenuLink>
                 </MenuItem>
                 <MenuItem>
-                   <MenuLink to="/contato">Contato</MenuLink>
+                    <MenuLink to="/contato">Contato</MenuLink>
                 </MenuItem>
             </MenuList>
         </MenuContainer>
