4a7974104b34f55890c40d26410bf255cfe23693
dos Santos <a.dos.santos@sap.com>
2024-02-17 00:45:32 -0300

    Rodapé finalizado

--- src/components/Rodape/rodape.jsx
+++ src/components/Rodape/rodape.jsx
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
-const ContainerRodape = styled.div `
+const RodapeContainer = styled.div `
     width: 100%;
     height: 120px;
     display: flex;
-    justfy-content: space-between;
+    justfy-content: left;
     align-items: center;
-    padding: 0 15px;
     background-color: #5F6F52;
     color: #FEFAE0;
+`
+const RodapeText = styled.p`
+  padding: 0 15px;
 `
 
 
 function Rodape() {
     return (
-        <ContainerRodape>
-            <h1>Isso é um rodapé</h1>
-        </ContainerRodape>
+        <RodapeContainer>
+            <RodapeText>Copyright &copy; 2024 | Site desenvolvido por AS.</RodapeText>
+        </RodapeContainer>
     )
 }
 
