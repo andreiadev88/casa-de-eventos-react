5c4f7c77b534df4b4b22a7e261b16d6e59ac63bc
dos Santos <a.dos.santos@sap.com>
2024-02-19 15:39:11 -0300

    react toastyfy

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import {
     LoginContainer,
@@ -17,13 +20,12 @@ import {
     SidebarContainer,
     LogoContainer,
     Logo,
-    ErrorMessage
+
 } from './login.styles'
 
 const Login = () => {
     const [nome, setNome] = useState('');
     const [senha, setSenha] = useState('');
-    const [erro, setErro] = useState('');
     const navigate = useNavigate();
 
     const EfetuarLogin = async () => {
@@ -33,9 +35,15 @@ const Login = () => {
             const usuarioEncontrado = usuarios.find(usuario => usuario.nome === nome && usuario.senha === senha);
 
             if (usuarioEncontrado) {
-                navigate('/homepage');
+                toast.success('Login efetuado com sucesso', {
+                    position: "top-rigth",
+                    autoClose: 3000,
+                });
             } else {
-                setErro('Nome ou senha invalido');
+                toast.error("Nome ou senha invalidos!", {
+                    position: "top-rigth",
+                    autoClose: 5000,
+                });
             }
 
         } catch (error) {
@@ -44,33 +52,33 @@ const Login = () => {
     };
     return (
         <LoginContainer>
-        <Sidebar>
-            <SidebarContainer>
-                <LogoContainer>
-                    <Logo src="../images/logo.svg" alt="Logomarca da casa de eventos" />
-                </LogoContainer>
-            </SidebarContainer>
-        </Sidebar>
-        <LoginContent>
-            <LoginBox>
-                <LoginTitle>Área de login</LoginTitle>
-                <FormBox>
-                    <FormItem>
-                        <FormLabel>Nome:</FormLabel>
-                        <FormInput type="text" placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
-                    </FormItem>
-                    <FormItem>
-                        <FormLabel>Senha:</FormLabel>
-                        <FormInput type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
-                    </FormItem>
-                    <FormItem>
-                        <FormButton onClick={EfetuarLogin}>Entrar</FormButton>
-                    </FormItem>
-                    {erro && <ErrorMessage>{erro}</ErrorMessage>}
-                </FormBox>
-            </LoginBox>
-        </LoginContent>          
-    </LoginContainer>
+            <Sidebar>
+                <SidebarContainer>
+                    <LogoContainer>
+                        <Logo src="../images/logo.svg" alt="Logomarca da casa de eventos" />
+                    </LogoContainer>
+                </SidebarContainer>
+            </Sidebar>
+            <LoginContent>
+                <LoginBox>
+                    <LoginTitle>Área de login</LoginTitle>
+                    <FormBox>
+                        <FormItem>
+                            <FormLabel>Nome:</FormLabel>
+                            <FormInput type="text" placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
+                        </FormItem>
+                        <FormItem>
+                            <FormLabel>Senha:</FormLabel>
+                            <FormInput type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
+                        </FormItem>
+                        <FormItem>
+                            <FormButton onClick={EfetuarLogin}>Entrar</FormButton>
+                        </FormItem>
+                        <ToastContainer />
+                    </FormBox>
+                </LoginBox>
+            </LoginContent>
+        </LoginContainer>
     );
 }
 export default Login
